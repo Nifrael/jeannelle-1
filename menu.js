08bf0c3599cf8b27a1727601140902b315ffec35
Julien Marchand <nifrael66@gmail.com>
2024-11-19 23:41:41 +0100

    bug fix

--- menu.js
+++ menu.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.querySelector(".menu");
-  const menuTitles = document.querySelector(".menu-titles");
+const menuToggle = document.querySelector(".menu");
+const menuTitles = document.querySelector(".menu-titles");
 
-  menuToggle.addEventListener("click", () => {
-    menuTitles.classList.toggle("show");
-  });
+menuToggle.addEventListener("click", () => {
+  menuTitles.classList.toggle("show");
 });
